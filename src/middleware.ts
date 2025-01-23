57e2598964df6c977283fab616598c515267b63b
Worratep1 <Worratepputed@gmail.com>
2025-01-24 00:14:41 +0700

    mail39

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,45 +1,25 @@
-import type { NextRequest } from 'next/server';
-import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
 import { User } from '@/types/user';
 
 export function middleware(request: NextRequest) {
-    // อ่านข้อมูล currentUser จาก Cookies
     const currentUser = request.cookies.get('currentUser')?.value;
-
     if (currentUser) {
         const currentUserData: User = JSON.parse(currentUser);
-        console.log("🚀 ~ middleware ~ currentUser:", currentUserData?.permission);
-    } else {
-        console.log("🔒 ~ middleware ~ No currentUser found");
+        // console.log("🚀 ~ middleware ~ currentUser:", currentUserData?.permission);
     }
 
-    // ตรวจสอบ Method ที่รองรับ (PUT, POST, GET)
-    const allowedMethods = ['PUT', 'POST', 'GET'];
-    if (!allowedMethods.includes(request.method)) {
-        return new NextResponse(
-            JSON.stringify({ message: `Method ${request.method} ไม่อนุญาต` }),
-            {
-                status: 405,
-                headers: { 'Content-Type': 'application/json' },
-            }
-        );
-    }
+    // console.log('request.nextUrl.pathname', request.nextUrl.pathname);
+    // if (currentUser && !request.nextUrl.pathname.startsWith('/admin/dashboard')) {
+    //     return NextResponse.redirect(new URL('/admin/dashboard', request.url));
+    // }
 
-    // ตรวจสอบเส้นทาง (Path) และ Redirect หากจำเป็น
-    const pathname = request.nextUrl.pathname;
-    if (currentUser && !pathname.startsWith('/admin/dashboard')) {
-        return NextResponse.redirect(new URL('/admin/dashboard', request.url));
-    }
-
-    if (!currentUser && !pathname.startsWith('/login')) {
-        return NextResponse.redirect(new URL('/login', request.url));
-    }
-
-    // อนุญาตให้คำขอผ่านไปยังหน้า หรือ API ต่อไป
-    return NextResponse.next();
+    // if (!currentUser && !request.nextUrl.pathname.startsWith('/login')) {
+    //     return Response.redirect(new URL('/login', request.url));
+    // }
 }
 
 export const config = {
-    // กำหนด matcher เพื่อกำหนดเส้นทางที่ Middleware จะทำงาน
-    matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'], // ใช้ regex เพื่อยกเว้นบางไฟล์หรือเส้นทาง
+    matcher: ['/((?!api|_next/static|_next/image|.*\.png$).*)'],
 };
+//ฟกหดฟกหดฟดฟ
