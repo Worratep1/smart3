376123bd570dd083b5bf651c49fdf3cd5d7c1dc0
Worratep1 <Worratepputed@gmail.com>
2025-02-23 14:36:27 +0700

    edit list and return

--- src/pages/api/borrowequipment/return.ts
+++ src/pages/api/borrowequipment/return.ts
@@ -65,3 +65,4 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
     return res.status(405).json({ message: `Method ${req.method} not allowed` });
   }
 }
+// File: /pages/api/borrowequipment/return.ts
