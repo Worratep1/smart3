cb7083e17f4b5bd6be0c1706f90dcb2c4387d164
Worratep1 <Worratepputed@gmail.com>
2025-04-03 15:08:40 +0700

    edit borrow 1

--- src/pages/borrowequipment/borrow.tsx
+++ src/pages/borrowequipment/borrow.tsx
@@ -190,9 +190,9 @@ const Borrow = () => {
             
                     {/* <TextareaLabel label='ที่อยู่' id="borrow_address" required /> */}
                     {/* <InputLabel label='หมายเลขโทรศัพท์' id="borrow_tel" required /> */}
-                    <br>
+                    
                     {<InputLabel label='ขอยืมครุภัณฑ์เพื่อ' id="borrow_objective" required />}
-                    </br>
+                    
                     <Form.Group>
                         <p className="m-0">วันเดือนปี (เริ่ม)</p>
                         <div className="py-2">
