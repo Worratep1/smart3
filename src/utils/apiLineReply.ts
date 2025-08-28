3dbd03abe96bc94b2490b2822390b58114aed515
Worratep1 <Worratepputed@gmail.com>
2025-08-28 11:19:37 +0700

    เปลี่ยนข้อความ  3 ไฟล์ sentHeartRate , senttemperature , apiLineReply

--- src/utils/apiLineReply.ts
+++ src/utils/apiLineReply.ts
@@ -1210,7 +1210,7 @@ export const replyNotification_Temperature = async ({
                                     contents: [
                                         {
                                             type      : "span",
-                                            text      : "แจ้งอุณหภูมิร่างกายสูง",
+                                            text      : "แจ้งอุณหภูมิร่างกาย",
                                             color     : "#FC0303",
                                             size      : "xl",
                                             weight    : "bold",
@@ -1366,7 +1366,7 @@ export const replyNotification_HeartRate = async ({
                                     contents: [
                                         {
                                             type      : "span",
-                                            text      : "แจ้งอัตราการเต้นชีพจรผิดปกติ",
+                                            text      : "แจ้งอัตราการเต้นชีพจร",
                                             color     : "#FC0303",
                                             size      : "xl",
                                             weight    : "bold",
@@ -1737,7 +1737,7 @@ export const replyNotificationPostbackTemp = async ({
                                     contents: [
                                         {
                                             type      : "span",
-                                            text      : "แจ้งอุณหภูมิร่างกายสูง",
+                                            text      : "แจ้งอุณหภูมิร่างกาย",
                                             color     : "#FC0303",
                                             size      : "xl",
                                             weight    : "bold",
@@ -1969,7 +1969,7 @@ export const replyNotificationPostbackHeartRate = async ({
                                     contents: [
                                         {
                                             type      : "span",
-                                            text      : "แจ้งอัตราการเต้นชีพจรผิดปกติ",
+                                            text      : "แจ้งอัตราการเต้นชีพจร",
                                             color     : "#FC0303",
                                             size      : "xl",
                                             weight    : "bold",
